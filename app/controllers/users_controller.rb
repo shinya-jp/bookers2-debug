32cbddbfc86b2775a8a335a0da4b92e5d7d367dd
shinya-jp <shinya0733@gmail.com>
2020-05-04 15:16:04 +0900

    [Finish]課題2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :baria_user, only: [:update]
+  before_action :authenticate_user!
 
   def show
   	@user = User.find(params[:id])
@@ -15,16 +16,16 @@ class UsersController < ApplicationController
   def edit
   	@user = User.find(params[:id])
     if @user != current_user
-      redirect_to books_path
+      redirect_to user_path(current_user.id)
     end
   end
 
   def update
   	@user = User.find(params[:id])
   	if @user.update(user_params)
-  		redirect_to users_path(@user), notice: "successfully updated user!"
+  		redirect_to user_path(@user), notice: "successfully updated user!"
   	else
-  		render "show"
+  		render "edit"
   	end
   end
 
